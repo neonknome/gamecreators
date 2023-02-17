7f1d0098ea4dbb7eccaa42c21bf2e0ee6f410917
neonknome <patrykialan@gmail.com>
2023-02-17 06:34:18 +0100

    Templates

---
+++ tools/xml/config.php
@@ -0,0 +1,25 @@
+<cartoonnetwork>
+<tunein>
+<text language="es">
+<![CDATA[ BEN 10: ULTIMATE ALIEN - NEW SHOW ]]>
+</text>
+<text language="pt">
+<![CDATA[ BEN 10: SUPREMACIA ALIENÍGENA - NOVO DESENHO ]]>
+</text>
+<text language="en">
+<![CDATA[ BEN 10: ULTIMATE ALIEN - NEW SHOW ]]>
+</text>
+</tunein>
+<servicePath>
+<![CDATA[ /do.service ]]>
+</servicePath>
+<staticPath>
+<![CDATA[./]]>
+</staticPath>
+<gameDataPath>
+<![CDATA[ http://i.cdn.turner.com/v5cache/CARTOON/gamecreator/images2/]]>
+</gameDataPath>
+<ssoPath>
+<![CDATA[ login.swf ]]>
+</ssoPath>
+</cartoonnetwork>
